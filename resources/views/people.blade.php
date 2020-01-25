62c81712737727159be2605be3655895591ad4dc
Umar <uhussain2011@gmail.com>
2020-01-25 20:19:56 +0000

    Bootstrap classes

--- resources/views/people.blade.php
+++ resources/views/people.blade.php
@@ -4,7 +4,10 @@
 
 @section('content')
 
-<h1> People CRUD </h1>
+
+
+<div class="container">
+<h1>Employees</h1>
 
 
 @if ($errors->any())
@@ -23,37 +26,48 @@
     </div>
 @endif
 
+<div class="row justify-content-center">
+    <div class="col-auto">
+
 <form action="{{route('people.store')}}" method="post">
     {{csrf_field()}}
-    <table>
+
+    <table class="table table-striped">
+    
         <tr>
-            <th>#</th>
-            <th>First name</th>
-            <th>Last name</th>
-            <th>Email Address</th>
-            <th>Job Role</th>
-            <th>Delete</th>
+            <th scope="col">#</th>
+            <th scope="col">First name</th>
+            <th scope="col">Last name</th>
+            <th scope="col">Email Address</th>
+            <th scope="col">Job Role</th>
+            <th scope="col">Delete</th>
         </tr>
+
         @foreach ($people as $id => $values)
             <tr>
-                <td>{{$loop->iteration}}<input type="hidden" name="{{'people['.$id.'][id]'}}"  value="{{$values->id}}" /></td>
-                <td><input type="text" name="{{'people['.$id.'][firstname]'}}"  value="{{$values->firstname}}" /></td>
-                <td><input type="text" name="{{'people['.$id.'][lastname]'}}"  value="{{$values->lastname}}" /></td>
-                <td><input type="text" name="{{'people['.$id.'][email]'}}"  value="{{$values->email}}" /></td>
-                <td><input type="text" name="{{'people['.$id.'][job_role]'}}"  value="{{$values->role}}" /></td>
-                <td><input  type="checkbox" name="{{'delete['.$id.']'}}" value="{{$values->id}}" /></td>
+                <th scope="row">{{$loop->iteration}}<input type="hidden" name="{{'people['.$id.'][id]'}}"  value="{{$values->id}}" /></th>
+                <td><input type="text" class="form-control" name="{{'people['.$id.'][firstname]'}}"  value="{{$values->firstname}}" /></td>
+                <td><input type="text" class="form-control" name="{{'people['.$id.'][lastname]'}}"  value="{{$values->lastname}}" /></td>
+                <td><input type="text" class="form-control" name="{{'people['.$id.'][email]'}}"  value="{{$values->email}}" /></td>
+                <td><input type="text" class="form-control" name="{{'people['.$id.'][job_role]'}}"  value="{{$values->role}}" /></td>
+                <td><input  type="checkbox" class="form-check-input mx-auto" name="{{'delete['.$id.']'}}" value="{{$values->id}}" /></td>
             </tr>
         @endforeach
         <tr>
         <tr>
             <td/>
-            <td><input type="text" name="firstname" placeholder="Add new..." value="{{old('firstname')}}" /></td>
-            <td><input type="text" name="lastname" placeholder="Add new..." value="{{old('lastname')}}" /></td>
-            <td><input type="text" name="email" placeholder="Add new..." value="{{old('email')}}" /></td>
-            <td><input type="text" name="job_role" placeholder="Add new..." value="{{old('job_role')}}" /></td>
+            <td><input type="text" class="form-control" name="firstname" placeholder="Add new..." value="{{old('firstname')}}" /></td>
+            <td><input type="text" class="form-control" name="lastname" placeholder="Add new..." value="{{old('lastname')}}" /></td>
+            <td><input type="text" class="form-control" name="email" placeholder="Add new..." value="{{old('email')}}" /></td>
+            <td><input type="text" class="form-control" name="job_role" placeholder="Add new..." value="{{old('job_role')}}" /></td>
         </tr>
     </table>
-    <input type="submit" value="Submit!" />
-</form>
 
+    <input type="submit" class="btn btn-primary float-right" value="Submit!" />
+</form>
+</div>
+</div>
+
+
+</div>
 @endsection
